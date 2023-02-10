6546f3145c21b0c5b8c80bf760b7ef3d2b9b755d
wirr <wirr@banana.local>
2023-02-10 13:51:51 +0100

    Redirect github references to custom repo and remove colorschemes

--- lua/lazyvim/plugins/core.lua
+++ lua/lazyvim/plugins/core.lua
@@ -2,5 +2,5 @@ require("lazyvim.config").init()
 
 return {
   { "folke/lazy.nvim", version = "*" },
-  { "LazyVim/LazyVim", priority = 10000, lazy = false, config = true, version = "*" },
+  { "wirr00/LazyVim", priority = 10000, lazy = false, config = true, version = "*" },
 }
